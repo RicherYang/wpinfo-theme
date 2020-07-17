b6164f34bc9b24afd03bba329266a223a4ee1e23
RicherYang <fantasyworldidvtw@gmail.com>
2020-07-18 01:07:06 +0800

    新增網站自動更新跳轉

--- assets/js/create.js
+++ assets/js/create.js
@@ -2,10 +2,11 @@
 
 jQuery(function ($) {
 
+    var data, recheckTimes;
     $('#add_url').on('click', function () {
         $('.url-info').addClass(['d-none']);
 
-        var data = $('#create_form').serializeObject();
+        data = $('#create_form').serializeObject();
         sendApi({
             url: ajaxInfo.url + '/check_url',
             data: data,
@@ -20,10 +21,38 @@ jQuery(function ($) {
                         alert(Jdata.info);
                     } else {
                         $info.removeClass(['d-none']);
+                        if (Jdata.info == 'confirming') {
+                            recheckTimes = 0;
+                            setTimeout(recheck, 2500);
+                        }
                     }
                 }
             }
         });
     });
+
+    function recheck() {
+        var checkData = $('#create_form').serializeObject();
+        if (JSON.stringify(checkData) == JSON.stringify(data)) {
+            recheckTimes += 1;
+            sendApi({
+                url: ajaxInfo.url + '/check_url',
+                data: data,
+                method: 'POST',
+                success: function (Jdata) {
+                    if (Jdata.url !== undefined) {
+                        window.location = Jdata.url;
+                    }
+                    if (Jdata.info !== undefined) {
+                        if (Jdata.info == 'confirming') {
+                            if (recheckTimes < 5) {
+                                setTimeout(recheck, 2500);
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
 
 });
